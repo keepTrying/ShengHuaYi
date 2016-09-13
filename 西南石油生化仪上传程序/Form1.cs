8af4d83d458ccc01606a7ba74d225da96e8385d6
KeepTrying <382858146@qq.com>
2016-09-13 17:22:05 +0800

    修复已知问题

--- 西南石油生化仪上传程序/Form1.cs
+++ 西南石油生化仪上传程序/Form1.cs
@@ -18,13 +18,15 @@ namespace 西南石油生化仪上传程序
         private String str_access_conn;
         private String str_access_path;
         private String str_access_copy;
+        private String doctor_id;
         private static String str_sqlsever_conn = @"server=192.168.1.252;database=jk2007;user=user26;pwd=welcome";
         private OleDbConnection access_conn;
         private SqlConnection sql_conn;
         private DataTable dt_name;
         private DataTable dt_item;
+        private DataTable dt_worker;
         private DataSet ds_access;
-        private int row_name;
+        private bool isMsgBox = false;//弹窗之前设为true，不执行refresh_access()方法
         public Form1()
         {
             
@@ -50,18 +52,21 @@ namespace 西南石油生化仪上传程序
             }
             catch
             {
+                isMsgBox = true;
                 MessageBox.Show("连接sql_sever数据库失败");
                 Environment.Exit(0);
             }
                        
             this.refresh_access();
 
-            if (dataGridView_name.Rows.Count == 0) {
-          
-                MessageBox.Show("没有这个时间段的本地数据！");
-            }
-        }
+            //if (dataGridView_name.Rows.Count == 0) {
 
+            //    isMsgBox = true;
+            //    MessageBox.Show("没有这个时间段的本地数据！");
+                
+            //}
+        }
+        //查询
         private void button3_Click(object sender, EventArgs e)
         {
             //dataGridView1.DataSource = null;
@@ -83,7 +88,6 @@ namespace 西南石油生化仪上传程序
                 return;
             }
             textBox_code.Focus();
-            row_name = row;
             int order = Convert.ToInt32(dataGridView_name.Rows[row].Cells[0].Value);
             String name = (String)dataGridView_name.Rows[row].Cells[1].Value;
             DateTime time = Convert.ToDateTime(dataGridView_name.Rows[row].Cells[2].Value);
@@ -128,10 +132,11 @@ namespace 西南石油生化仪上传程序
             }
             catch
             {
+                isMsgBox = true;
                 MessageBox.Show("连接Access数据库失败");
                 Environment.Exit(0);
             }
-
+            if (access_conn.State != ConnectionState.Open) access_conn.Open();
             OleDbDataAdapter da;
 
             dt_name = new DataTable();
@@ -185,11 +190,26 @@ namespace 西南石油生化仪上传程序
             DataTable dt_itemNo = new DataTable();
             sqlda.Fill(dt_itemNo);
 
+            dt_worker = new DataTable();
+            sql="select 编号,姓名 from 系统管理_员工基本信息表";
+            sqlda = new SqlDataAdapter(sql, sql_conn);
+            sqlda.Fill(dt_worker);
+
             ds_access.Tables["join"].Columns.Add("ItemName");
             ds_access.Tables["join"].Columns.Add("ResDesc");
             ds_access.Tables["join"].Columns.Add("ItemCode");
+            ds_access.Tables["join"].Columns.Add("DoctorID");
             foreach (DataRow row in ds_access.Tables["join"].Rows)
             {
+                foreach(DataRow row_worker in dt_worker.Rows)
+                {
+                    if (row_worker[1] + "" == row["dt.Name"] + "")
+                    {
+                        row["DoctorID"] = row_worker[0] + "";
+                        doctor_id = row_worker[0] +"";
+                        break;
+                    }
+                }
                 int item_id = Convert.ToInt32(row["ItemID"]);
                 switch (Convert.ToInt32(row["ItemType"]))
                 {
@@ -298,30 +318,32 @@ namespace 西南石油生化仪上传程序
             }
             if (dataGridView_name.Rows.Count > 0)
             {
-                int order = Convert.ToInt32(dataGridView_name.Rows[0].Cells[0].Value);
-                String name = (String)dataGridView_name.Rows[0].Cells[1].Value;
-                DateTime time = Convert.ToDateTime(dataGridView_name.Rows[0].Cells[2].Value);
-                dt_item = new DataTable();
-                dt_item.Columns.Add("项目");
-                dt_item.Columns.Add("结果");
-                dt_item.Columns.Add("结论");
-                foreach (DataRow ro in ds_access.Tables["join"].Rows)
-                {
-                    if (Convert.ToString(ro["pi.Name"]) == name && Convert.ToDateTime(ro["TestTime"]) == time && Convert.ToInt32(ro["SampleID"]) == order)
-                    {
-                        dt_item.LoadDataRow(new Object[] { ro["ItemName"], ro["TestResult"], ro["ResDesc"] }, true);
-                    }
-                }
-                dt_item.AcceptChanges();
-                dataGridView_data.DataSource = null;
-                dataGridView_data.DataSource = dt_item;
-                dataGridView_data.Columns[0].FillWeight = 50;
-                dataGridView_data.Columns[1].FillWeight = 30;
-                dataGridView_data.Columns[2].FillWeight = 20;
+                //int order = Convert.ToInt32(dataGridView_name.Rows[0].Cells[0].Value);
+                //String name = (String)dataGridView_name.Rows[0].Cells[1].Value;
+                //DateTime time = Convert.ToDateTime(dataGridView_name.Rows[0].Cells[2].Value);
+                //dt_item = new DataTable();
+                //dt_item.Columns.Add("项目");
+                //dt_item.Columns.Add("结果");
+                //dt_item.Columns.Add("结论");
+                //foreach (DataRow ro in ds_access.Tables["join"].Rows)
+                //{
+                //    if (Convert.ToString(ro["pi.Name"]) == name && Convert.ToDateTime(ro["TestTime"]) == time && Convert.ToInt32(ro["SampleID"]) == order)
+                //    {
+                //        dt_item.LoadDataRow(new Object[] { ro["ItemName"], ro["TestResult"], ro["ResDesc"] }, true);
+                //    }
+                //}
+                //dt_item.AcceptChanges();
+                //dataGridView_data.DataSource = null;
+                //dataGridView_data.DataSource = dt_item;
+                //dataGridView_data.Columns[0].FillWeight = 50;
+                //dataGridView_data.Columns[1].FillWeight = 30;
+                //dataGridView_data.Columns[2].FillWeight = 20;
+                updateDisplay();
             }
             else
             {
-                //MessageBox.Show("没有这个时间段的本地数据！");
+                isMsgBox = true;
+                MessageBox.Show("没有这个时间段的本地数据！");
             }
 
 
@@ -341,6 +363,7 @@ namespace 西南石油生化仪上传程序
             }
             catch
             {
+                isMsgBox = true;
                 MessageBox.Show("编码必须为数字！");
                 return;
             }
@@ -370,24 +393,34 @@ namespace 西南石油生化仪上传程序
             }
             catch
             {
+                isMsgBox = true;
                 MessageBox.Show("SQL_SEVER服务器上没有本编号的数据！");
                 return;
             }
             if (dt.Rows.Count == 0)
             {
+                isMsgBox = true;
                 MessageBox.Show("SQL_SEVER服务器上没有本编号的数据！");
                 return;
             }
 
             dataGridView_person.DataSource = null;
             dataGridView_person.DataSource = dt;
-            if (dataGridView_name.Rows.Count > 0)
+            updateDisplay();
+        }
+        /// <summary>
+        /// 在姓名列表中选中体检者，并在结果列表中显示体检结果
+        /// </summary>
+        private void updateDisplay()
+        {
+            if (dataGridView_name.Rows.Count > 0 && dataGridView_person.Rows.Count>0)
             {
-                for(int i = 0; i < dataGridView_name.Rows.Count; i++)
+                for (int i = 0; i < dataGridView_name.Rows.Count; i++)
                 {
-                    if (dataGridView_name[i, 1].Value.ToString() == dataGridView_person[0,0].Value.ToString())
+                    if (dataGridView_name[1, i].Value.ToString() == dataGridView_person[0, 0].Value.ToString())
                     {
-                        dataGridView_name.Rows[i].Selected=true;
+                        dataGridView_name.FirstDisplayedScrollingRowIndex = i;
+                        dataGridView_name.Rows[i].Selected = true;
 
                         int order = Convert.ToInt32(dataGridView_name.Rows[i].Cells[0].Value);
                         String name = (String)dataGridView_name.Rows[i].Cells[1].Value;
@@ -409,28 +442,35 @@ namespace 西南石油生化仪上传程序
                         dataGridView_data.Columns[0].FillWeight = 50;
                         dataGridView_data.Columns[1].FillWeight = 30;
                         dataGridView_data.Columns[2].FillWeight = 20;
+
+                        break;
                     }
                 }
-            }    
+            }
         }
 
+        //上传
         private void button2_Click(object sender, EventArgs e)
         {
             if (textBox_code.Text.Trim() == "")
             {
+                isMsgBox = true;
                 MessageBox.Show("请扫描条码");
                 return;
             }
 
-            if (dataGridView_name.SelectedRows==null)
+            if (dataGridView_name.SelectedRows.Count<1)
             {
+                isMsgBox = true;
                 MessageBox.Show("请选择要上传的数据");
                 return;
             }
 
-            int order = Convert.ToInt32(dataGridView_name.Rows[row_name].Cells[0].Value);
-            String name = (String)dataGridView_name.Rows[row_name].Cells[1].Value;
-            DateTime time = Convert.ToDateTime(dataGridView_name.Rows[row_name].Cells[2].Value);
+            int order = Convert.ToInt32(dataGridView_name.SelectedRows[0].Cells[0].Value);
+            String name = (String)dataGridView_name.SelectedRows[0].Cells[1].Value;
+            DateTime time = Convert.ToDateTime(dataGridView_name.SelectedRows[0].Cells[2].Value);
+            string conclusion = "";
+            string dct_name = "";
             foreach (DataRow ro in ds_access.Tables["join"].Rows)
             {
                 if (Convert.ToString(ro["pi.Name"]) == name && Convert.ToDateTime(ro["TestTime"]) == time && Convert.ToInt32(ro["SampleID"]) == order)
@@ -447,11 +487,14 @@ namespace 西南石油生化仪上传程序
                             break;
                         case "1":
                             res = "阳性";
+                            conclusion += name + "呈" + res+";";
                             break;
                         case "2":
                             res = "弱阳性";
+                            conclusion += name + "呈" + res + ";";
                             break;
                         default:
+                            if(Convert.ToString(ro["resDesc"])=="偏高"|| Convert.ToString(ro["resDesc"]) == "偏低") conclusion += ro["ItemName"] +"检测结果"+ Convert.ToString(ro["resDesc"]) + ";";
                             break;
                     }
                     String sql;
@@ -462,13 +505,13 @@ namespace 西南石油生化仪上传程序
                     if (!read.HasRows)
                     {
                         read.Close();
-                        sql = "insert 职业病体检_结果信息_生化科(系统编号,体检项目,体检结果,体检医师,填写时间,单项结论) values('" + textBox_code.Text.Trim() + "','" + ro["ItemCode"] + "','" + res + "','" + ro["dt.Name"] + "','" + ro["TestTime"] + "','" + ro["ResDesc"] + "')";
+                        sql = "insert 职业病体检_结果信息_生化科(系统编号,体检项目,体检结果,体检医师,填写时间,单项结论) values('" + textBox_code.Text.Trim() + "','" + ro["ItemCode"] + "','" + res + "','" + ro["DoctorID"] + "','" + ro["TestTime"] + "','" + ro["ResDesc"] + "')";
                         cmd = new SqlCommand(sql, sql_conn);
                         cmd.ExecuteNonQuery();
                     }
                     else {
                         read.Close();
-                        sql = "delete from 职业病体检_结果信息_生化科 where 系统编号='" + textBox_code.Text.Trim() + "';insert 职业病体检_结果信息_生化科(系统编号,体检项目,体检结果,体检医师,填写时间,单项结论) values('" + textBox_code.Text.Trim() + "','" + ro["ItemCode"] + "','" + res + "','" + ro["dt.Name"] + "','" + ro["TestTime"] + "','" + ro["ResDesc"] + "')";
+                        sql = "delete from 职业病体检_结果信息_生化科 where 系统编号='" + textBox_code.Text.Trim() + "';insert 职业病体检_结果信息_生化科(系统编号,体检项目,体检结果,体检医师,填写时间,单项结论) values('" + textBox_code.Text.Trim() + "','" + ro["ItemCode"] + "','" + res + "','" + ro["DoctorID"] + "','" + ro["TestTime"] + "','" + ro["ResDesc"] + "')";
                         //sql = "update 职业病体检_结果信息_生化科 set 体检结果='" + res + "',体检医师='" + ro["dt.Name"] + "',填写时间='" + ro["TestTime"] + "',单项结论='" + ro["ResDesc"] + "' where 系统编号='" + textBox_code.Text.Trim() + "' and 体检项目='" + ro["ItemCode"] + "'";
                         cmd = new SqlCommand(sql, sql_conn);
                         cmd.ExecuteNonQuery();
@@ -478,6 +521,28 @@ namespace 西南石油生化仪上传程序
 
                 }
             }
+            if (conclusion == "") conclusion = "未见异常";
+ 
+            String sql2 = "select * from 职业病体检_科室结论表 where 系统编号='" + textBox_code.Text.Trim() + "' and 科室='17'";
+            SqlCommand cmd2 = new SqlCommand(sql2, sql_conn);
+            SqlDataReader read2 = cmd2.ExecuteReader();
+            if (!read2.HasRows)
+            {
+                read2.Close();
+                sql2 = "insert 职业病体检_科室结论表(系统编号,科室,文字结论,医生编号,结论日期,修改起始时间) values('" + textBox_code.Text.Trim() + "','17','" + conclusion + "','" +doctor_id + "','" + time + "','" + DateTime.Now.ToString()+"')";
+                cmd2 = new SqlCommand(sql2, sql_conn);
+                cmd2.ExecuteNonQuery();
+            }
+            else
+            {
+                read2.Close();
+                sql2 = "delete from 职业病体检_科室结论表 where 系统编号='" + textBox_code.Text.Trim() + "' and 科室='17';insert 职业病体检_科室结论表(系统编号,科室,文字结论,医生编号,结论日期,修改起始时间) values('" + textBox_code.Text.Trim() + "','17','" + conclusion + "','" + doctor_id + "','" + time + "','" + DateTime.Now.ToString()+"')";
+                cmd2 = new SqlCommand(sql2, sql_conn);
+                cmd2.ExecuteNonQuery();
+
+            }
+
+            isMsgBox = true;
             DialogResult dialogRes = MessageBox.Show("上传成功");
             if (dialogRes == DialogResult.OK) {
                 textBox_code.Clear();
@@ -493,6 +558,11 @@ namespace 西南石油生化仪上传程序
 
         private void Form1_Activated(object sender, EventArgs e)
         {
+            if (isMsgBox)
+            {
+                isMsgBox = false;
+                return;
+            }
             this.refresh_access();
         }
 
